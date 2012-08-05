a6aebc42c514e4ac409b88e2a277ef4311d623d9
Zhang Xin <zhangxin.02@gmail.com>
2012-08-05 16:38:23 +0800

    Add word "CONSTANT"

--- test.fs
+++ test.fs
@@ -1,11 +1,12 @@
-0x13 CONST ten
-END
+0x13 CONSTANT ten
+0x40000080 CONSTANT test
+
 : init-uart
-    ten ten !    
+    HERE test !   
 ;
 : init-uart1
     init-uart
-    0x89 0x400000A0 !    
+    ten 0x400000A0 !    
 ;
 
 END  init-uart1
