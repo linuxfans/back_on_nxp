c93bd3395ddc04235082c626e417856f65222036
Zhang Xin <zhangxin.02@gmail.com>
2012-08-05 17:08:58 +0800

    Add word "VARIABLE"

--- test.fs
+++ test.fs
@@ -1,12 +1,16 @@
 0x13 CONSTANT ten
-0x40000080 CONSTANT test
-
+0x40000080 CONSTANT test1
+0x400000A0 CONSTANT test2
+VARIABLE myvar1
+VARIABLE myvar2
 : init-uart
-    HERE test !   
+    ten myvar1 !
+    ten ten + myvar2 !
+    myvar1 @ test1 !
+    myvar2 @ test2 !    
 ;
 : init-uart1
     init-uart
-    ten 0x400000A0 !    
 ;
 
 END  init-uart1
